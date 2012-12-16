cc95910a593ecc2c1cebac611469f2a0099b0729
Matt Jackson <me@mattjackson.eu>
2012-12-16 12:14:54 +0000

    refactored tiles into their own class

--- wordsmush/wordsmush.py
+++ wordsmush/wordsmush.py
@@ -6,69 +6,126 @@ from colorama import Fore, Back, Style
 from wordsmush.word_lookup import words
 
 
-# tile statuses
-UNTAKEN = 0
-TAKEN_PLAYER1 = 1
-TAKEN_PLAYER2 = 2
-PROTECTED_PLAYER1 = 3
-PROTECTED_PLAYER2 = 4
 
 class WordsmushGame(object):
 
     def __init__(self, player1, player2, board_width=5, board_height=5):
+        self.board_width = 5
+        self.board_height = 5
 
         self.player1 = player1
         self.player2 = player2
 
         random_letter = lambda: chr(randint(ord('a'), ord('z')))
-        self.board = [[ (random_letter(), UNTAKEN) for n in range(board_width) ] 
-                            for m in range(board_height) ] 
+        self.board = [[ WordsmushTile(self, n, m, random_letter())
+                            for n in range(board_width)] for m in range(board_height)]
         self.letters = [letter for letter, status in reduce(add, self.board)]
+
+    @property
+    def tiles(self):
+        """Iterator for all tiles on the board"""
+        for tile in reduce(add, self.board):
+            yield tile
 
     def __repr__(self):
         return self.format_pretty()
 
     def format_pretty(self):
         COLOR_MAPPING = {
-            UNTAKEN: Fore.BLACK + Back.WHITE + Style.NORMAL,
-            TAKEN_PLAYER1: Fore.CYAN + Back.BLUE + Style.DIM, 
-            TAKEN_PLAYER2: Fore.RED + Back.YELLOW + Style.DIM,
-            PROTECTED_PLAYER1: Fore.BLUE + Back.BLUE + Style.BRIGHT,
-            PROTECTED_PLAYER2: Fore.RED + Back.RED + Style.BRIGHT,
+            (WordsmushTile.UNTAKEN, None): Fore.BLACK + Back.WHITE + Style.NORMAL,
+            (WordsmushTile.TAKEN, self.player1): Fore.CYAN + Back.BLUE + Style.DIM, 
+            (WordsmushTile.TAKEN, self.player2): Fore.RED + Back.YELLOW + Style.DIM,
+            (WordsmushTile.PROTECTED, self.player1): Fore.BLUE + Back.BLUE + Style.BRIGHT,
+            (WordsmushTile.PROTECTED, self.player2): Fore.RED + Back.RED + Style.BRIGHT,
         }
 
         format_string = ''
         for tile_row in self.board:
-            for tile, status in tile_row:
-                format_string += COLOR_MAPPING[status] + (' %s ' % tile.upper())
+            for tile in tile_row:
+                format_string += COLOR_MAPPING[tile.status, tile.owner] + (' %s ' % tile.upper())
             format_string += Fore.RESET + Back.RESET + Style.RESET_ALL + '\n'
 
         return format_string
 
+    def get_tile(self, x, y):
+        """Get tile at the specified index
+        :param x: zero-based index of the x value of the requested tile
+        :param y: zero-based index of the y value of the requested tile"""
+
+        return self.board[y][x]
+
     def play(self, player, word):
-
-        taken_status = {self.player1: TAKEN_PLAYER1,
-                        self.player2: TAKEN_PLAYER2}
-
-        protected_status = {self.player1: PROTECTED_PLAYER1,
-                            self.player2: PROTECTED_PLAYER2}
 
         if self.is_a_word(word): 
 
             # give possession of each new letter to player, 
             # if the tiles are unprotected
-            for tile_x, tile_y, letter in word.tiles:
-                letter, status = self.board[tile_y][tile_x]
-                if status not in (PROTECTED_PLAYER1, PROTECTED_PLAYER2):
-                    self.board[tile_y][tile_x] = (letter, taken_status[player]) 
+            for tile in word.tiles:
+                if tile.status != WordsmushTile.PROTECTED:
+                    tile.status = WordsmushTile.TAKEN
+                    tile.owner = player
 
 
             # now recalculate protected status of all tiles
-            # TODO
-                
+            has_tile = lambda f: f() is not None
+            tile_owned = lambda t, p: t.status in (WordsmushTile.TAKEN, WordsmushTile.PROTECTED) and t.owner == p
+            def tile_surrounded(tile):
+                if ((not has_tile(tile.tile_above) or tile_owned(tile.tile_above(), tile.owner)) and
+                   (not has_tile(tile.tile_below) or tile_owned(tile.tile_above(), tile.owner)) and
+                   (not has_tile(tile.tile_left) or tile_owned(tile.tile_above(), tile.owner)) and
+                   (not has_tile(tile.tile_right) or tile_owned(tile.tile_right(), tile.owner))):
+                        return True
+
+            for tile in self.tiles:
+                if tile.status in (WordsmushTile.TAKEN, WordsmushTile.PROTECTED):
+                    if tile_surrounded(tile):
+                        tile.status = WordsmushTile.PROTECTED
+                    else:
+                        tile.status = WordsmushTile.TAKEN
+                    
 
     def is_a_word(self, word):
         return word.word in words
+
+class WordsmushTile(object):
+
+    # tile statuses
+    UNTAKEN = 0
+    TAKEN = 1
+    PROTECTED = 2
+
+    def __init__(self, game, x, y, letter):
+        self.game = game
+        self.x = x
+        self.y = y
+        self.letter = letter
+        self.status = self.UNTAKEN
+        self.owner = None
+
+    def tile_above(self):
+        if self.y == 0:
+            return None
+        else:
+            return self.game.get_tile(self.x, self.y-1) 
+
+    def tile_below(self):
+        if self.y+1 == self.game.board_height:
+            return None
+        else:
+            return self.game.get_tile(self.x, self.y+1) 
+
+    def tile_left(self):
+        if self.x == 0:
+            return None
+        else:
+            return self.game.get_tile(self.x-1, self.y)
+
+    def tile_right(self):
+        if self.x+1 == self.game.board_width:
+            return None
+        else:
+            return self.game.get_tile(self.x+1, self.y)
+
 
 class WordsmushWord(object):
     
@@ -78,7 +135,10 @@ class WordsmushWord(object):
 
     def add_tile(self, tile, position=None):
         """Add a tile to the word, optionally with a specific position.
-        This method may also be move a tile already in play. """
+        This method may also be move a tile already in play. 
+        :param tile: a WordsmushTile instance representing the tile to be added.
+        :param position: Optional argument to add the tile at a specific position.
+        When no position is specified, the tile is added to the end of the word"""
 
         if tile in self.tiles:
             self.tiles.remove(tile)
@@ -90,7 +150,7 @@ class WordsmushWord(object):
 
     @property
     def word(self):
-        return ''.join(letter for x, y, letter in self.tiles)
+        return ''.join(tile.letter for tile in self.tiles)
         
 
 class WordsmushPlayer(object):
